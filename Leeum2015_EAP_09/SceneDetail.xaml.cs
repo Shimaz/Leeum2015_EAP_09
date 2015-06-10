4fca80f3dc5590ba7f0965b7e29feb2c09e5508c
Shimaz <shimaz.bsh@gmail.com>
2015-06-10 21:57:05 +0900

    working 0610 basic structure

--- Leeum2015_EAP_09/SceneDetail.xaml.cs
+++ Leeum2015_EAP_09/SceneDetail.xaml.cs
@@ -20,9 +20,83 @@ namespace Leeum2015_EAP_09
     /// </summary>
     public partial class SceneDetail : UserControl
     {
-        public SceneDetail()
+
+        
+        private int detailLang;
+        private int ShowingImageNumber;
+        public SceneDetail(int lang, int imgNo)
         {
             InitializeComponent();
+            this.detailLang = lang;
+            this.ShowingImageNumber = imgNo;
+
+
+            InitContent(detailLang, ShowingImageNumber);
+
+        }
+
+
+        private void InitContent(int lang, int imgNo)
+        {
+            TextBlock tmpTb = new TextBlock();
+            tmpTb.FontSize = 50;
+            tmpTb.Foreground = Brushes.White;
+            Canvas.SetTop(tmpTb, 800);
+            Canvas.SetLeft(tmpTb, 520);
+
+            tmpTb.Text = "" + imgNo;
+
+
+
+            _cvBaseDetail.Children.Add(tmpTb);
+
+
+            Button btnTmp = new Button();
+            btnTmp.Content = "close";
+            btnTmp.Click += new RoutedEventHandler(btnCloseClicked);
+
+            btnTmp.Width = 200;
+            btnTmp.Height = 200;
+
+            Canvas.SetTop(btnTmp, 50);
+            Canvas.SetLeft(btnTmp, 50);
+
+            _cvBaseDetail.Children.Add(btnTmp);
+
+
+
+        }
+
+
+        private void showNextImg(object sender, RoutedEventArgs e)
+        {
+
+
+
+        }
+
+        private void showPrevImg(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        
+
+
+        private void setLanguage(int lang)
+        {
+
+
+        }
+
+        private void btnCloseClicked(object sender, RoutedEventArgs e)
+        {
+            ((Canvas)this.Parent).Children.Remove(this);
+
         }
     }
+
+
+
 }
--- Leeum2015_EAP_09/SceneHome.xaml.cs
+++ Leeum2015_EAP_09/SceneHome.xaml.cs
@@ -21,20 +21,47 @@ namespace Leeum2015_EAP_09
     public partial class SceneHome : UserControl
     {
 
-        private int lang_detail;
+        private int lang_home;
 
         public SceneHome(int lang)
         {
             InitializeComponent();
 
-            this.lang_detail = lang;
+            this.lang_home = lang;
 
-            InitContents(lang_detail);
+            InitContents(lang_home);
         }
 
         private void InitContents(int lang)
         {
+            for (int i = 0; i < 11; i++)
+            {
+                string num = string.Format("{0:00}", i + 1);
+                ImageButton btn = new ImageButton();
+                btn.NormalImage = new BitmapImage(new Uri("pack://application:,,,/Resources/btn_" + num + "_normal.png"));
+                btn.PushedImage = new BitmapImage(new Uri("pack://application:,,,/Resources/btn_" + num + "_pressed.png"));
 
+                if (i < 6)
+                {
+                    Canvas.SetLeft(btn, 100 + (i * 150));
+                    Canvas.SetTop(btn, 1400);
+
+
+                }
+                else
+                {
+                    Canvas.SetLeft(btn, 100 + (( i - 6) * 150));
+                    Canvas.SetTop(btn, 1600);
+
+                }
+
+
+                btn.Click += new RoutedEventHandler(btnClicked);
+
+                btn.Tag = 100 + i;
+                _cvBaseHome.Children.Add(btn);
+                
+            }
 
 
 
@@ -43,8 +70,14 @@ namespace Leeum2015_EAP_09
 
         private void btnClicked(object sender, RoutedEventArgs e)
         {
+            int imgNo = (int)((ImageButton)sender).Tag - 100;
 
+            SceneDetail scn = new SceneDetail(lang_home, imgNo);
 
+            Canvas.SetTop(scn, 0);
+            Canvas.SetLeft(scn, 0);
+
+            _cvBaseHome.Children.Add(scn);
 
         }
     }
